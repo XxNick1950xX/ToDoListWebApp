e055f34869b68bbc7c2b343e3ad6decbde1e5237
Nicholas Guohao Chen <nicholas.guohao.chen@avanade.com>
2023-09-12 15:07:15 +0800

    Add project files.

---
+++ ToDoListWebApp/Service/ToDoService.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ToDoListWebApp.Models;
+
+namespace ToDoListWebApp.Service
+{
+    public class ToDoService
+    {
+        public readonly ToDoContext context;
+        public ToDoService(ToDoContext context) 
+        {
+            this.context = context;
+        }
+
+        public async Task<List<ToDo>> GetToDosAsync()
+        {
+            return await context.ToDos.ToListAsync();
+        }
+
+        public async Task AddAsync(ToDo toDo)
+        {
+            await context.ToDos.AddAsync(toDo);
+            await context.SaveChangesAsync();
+        }
+    }
+}
